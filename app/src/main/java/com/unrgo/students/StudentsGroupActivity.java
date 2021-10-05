444e491eb18c3da1baea055d0a1b31622259ed4a
Sergey Dychko <dychkosergey@gmail.com>
2021-10-05 19:33:06 +0300

    lb5 layouts

---
+++ app/src/main/java/com/unrgo/students/StudentsGroupActivity.java
@@ -0,0 +1,14 @@
+package com.unrgo.students;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentsGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_students_group);
+    }
+}
