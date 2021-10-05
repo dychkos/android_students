9847bb5f9d28573996ef0fa84ae1a59820cb261b
Sergey Dychko <dychkosergey@gmail.com>
2021-10-05 18:27:40 +0300

    lb4 - life cycle

--- app/src/main/java/com/unrgo/students/MainActivity.java
+++ app/src/main/java/com/unrgo/students/MainActivity.java
@@ -1,19 +1,50 @@
 package com.unrgo.students;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    private int seconds;
+    private Boolean isRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState != null){
+            seconds = savedInstanceState.getInt("timer");
+        }
+        runTimer();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        isRunning = true;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        isRunning = false;
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("timer",seconds);
     }
 
     public void onBtnClick(View view){
@@ -23,5 +54,24 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("groupNumber",grpNum);
         startActivity(intent);
 
+    }
+
+    private void runTimer(){
+        final TextView timeView = (TextView)findViewById(R.id.timer);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int sesc = seconds%60;
+                String time = String.format(Locale.getDefault(),"%d:%02d:%02d",hours,minutes,sesc);
+                timeView.setText(time);
+                if(isRunning){
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
     }
 }
--- app/src/main/java/com/unrgo/students/StudentsActivity.java
+++ app/src/main/java/com/unrgo/students/StudentsActivity.java
@@ -1,13 +1,17 @@
 package com.unrgo.students;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
 
 public class StudentsActivity extends AppCompatActivity {
+
+    private  float textSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,15 @@ public class StudentsActivity extends AppCompatActivity {
 
         TextView textView  = (TextView) findViewById(R.id.text);
         textView.setText(txtStudents.toString());
+
+        textSize = textView.getTextSize();
+
+        if(savedInstanceState != null){
+            textSize = savedInstanceState.getFloat("textSize");
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_PX,textSize);
+
+
+        }
     }
 
     public void onBtnSendClick(View view){
@@ -35,5 +48,16 @@ public class StudentsActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void onBtnPlusClick(View view){
+        textSize *=1.1f;
+        TextView textView = findViewById(R.id.text);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_PX,textSize);
 
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putFloat("textSize",textSize);
+    }
 }
